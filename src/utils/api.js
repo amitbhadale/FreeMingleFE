b3175b50c21651c847eb40aaf5a44a68ac0f0d53
amitbhadale <amitbhadale156@gmail.com>
2025-04-30 23:32:56 +0530

    backedn url setup

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 // import { useToast } from 'vue-toastification';
 
 const apiClient = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_BACKEND_API_URL + '/api',
   withCredentials: true,
   // headers: { 'Content-Type': 'application/json' },
 })
