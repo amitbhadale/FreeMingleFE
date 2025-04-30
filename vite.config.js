5129fb1812f6d8e85a0f51b55008f0e4f9faf46a
amitbhadale <amitbhadale156@gmail.com>
2025-04-30 23:23:15 +0530

    env var change

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,28 @@
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools()],
-  server: {
-    proxy: {
-      '/api': {
-        target: import.meta.env.BACKEND_API_URL,
-        changeOrigin: true,
-        secure: false, // Set to false if backend is running on HTTP
+export default defineConfig(({ mode }) => {
+  // Load .env files based on mode (development, production, etc.)
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [vue(), vueJsx(), vueDevTools()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_API_URL, // use env prefix VITE_
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+  }
 })
